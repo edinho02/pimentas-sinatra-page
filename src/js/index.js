0c947dec2538c23461969a9fe697acf1fd899b6b
Edson <edinho_andreassa@outlook.com>
2022-11-02 20:21:08 -0300

    links fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
-document.querySelectorAll('.link-btn').forEach(btn => {
+document.querySelectorAll('[lk]').forEach(btn => {
   btn.addEventListener('click', e => {
+    e.preventDefault();
+
     const link = btn.getAttribute('lk');
     if (!link) {
       return;
     }
+
     window.open(link, '_system');
   });
 });
